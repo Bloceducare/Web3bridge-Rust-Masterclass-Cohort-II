d4a61651897c476483174ee2e5b24e1f3daac1b0
Emeka Allison <emekaallison4@gmail.com>
2026-02-09 12:17:37 +0100

    Clean up and refactors here and there

--- submissions/week-2/day-5/emeka-expense-tracker/src/record.rs
+++ submissions/week-2/day-5/emeka-expense-tracker/src/record.rs
@@ -2,8 +2,10 @@ use crate::expenses::Expense;
 use std::fs::File;
 use std::io::{Result, Write};
 
+const REPORT_FILE: &str = "report.txt";
+
 pub fn write_to_report(expenses: &Vec<Expense>, total: f64) -> Result<()> {
-    let mut file = File::create("report.txt")?;
+    let mut file = File::create(REPORT_FILE)?;
     for (index, expense) in expenses.iter().enumerate() {
         let items_str = expense
             .items
